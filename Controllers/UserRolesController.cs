65e17a3517d65d0da4c8fc26a4f692b87acf1fb8
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-09-18 00:04:20 -0500

    Changes Made More To Come

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -2,13 +2,16 @@
 using BugTracker.Models;
 using BugTracker.Models.ViewModels;
 using BugTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BugTracker.Controllers
 {
+    [Authorize]
     public class UserRolesController : Controller
     {
         private readonly IBugTrackerRolesService _rolesService;
@@ -20,7 +23,8 @@ namespace BugTracker.Controllers
             _companyInfoService = companyInfoService;
         }
 
-        public async Task<IActionResult> ManagerUserRoles()
+        [HttpGet]
+        public async Task<IActionResult> ManageUserRoles()
         {
             // Add An Instance Of The ViewModel As A List (model)
             List<ManageUserRolesViewModel> model = new();
@@ -44,8 +48,44 @@ namespace BugTracker.Controllers
                 model.Add(viewModel);
             }
             
-            // Return The Model To The View;
+            // Return The Model To The View
             return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+            // Get The CompanyId
+            int comapanyId = User.Identity.GetCompanyId().Value;
+
+            // Instantiate The BugTrackerUser
+            BugTrackerUser bugTrackerUser = (await _companyInfoService.GetAllMembersAsync(comapanyId)).FirstOrDefault(u => u.Id == member.BugTrackerUser.Id);
+
+
+            // Get Roles Of The User
+            IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(bugTrackerUser);
+
+            // Grab The Selected Role
+            string userRole = member.SelectedRoles.FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(userRole))
+            {
+                // Remove The User From The Roles
+                if (await _rolesService.RemoveUserFromRolesAsync(bugTrackerUser, roles))
+                {
+                    // Add User To The New Role
+                    await _rolesService.AddUserToRoleAsync(bugTrackerUser, userRole);
+                }
+
+            }
+
+
+
+            // Navigate Back To The View
+
+            return RedirectToAction(nameof(ManageUserRoles));
         }
     }
 }
