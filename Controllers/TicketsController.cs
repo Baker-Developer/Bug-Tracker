6b9fcee19ae162c8044857265ceb40520e95dffc
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-09-23 23:16:02 -0500

    More Changes

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -66,7 +66,13 @@ namespace BugTracker.Controllers
 
         }
 
+        public async Task<IActionResult> ArchivedTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+            List<Ticket> tickets = await _ticketService.GetArchivedTicketsAsync(companyId);
 
+            return View(tickets);
+        }
 
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
